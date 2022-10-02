7624818a790638c5508803b48c4dc0f8d6176966
segreaves <simurs@yahoo.com>
2022-10-02 15:04:46 -0400

    Added movement scriptable object data to AgentMovement.
    Acceleration and deceleration added to movement.

--- WATD/Assets/_Scripts/AgentMovement.cs
+++ WATD/Assets/_Scripts/AgentMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,7 @@ public class AgentMovement : MonoBehaviour
     [field: SerializeField] public MovementDataSO MovementData { get; set; }
 
     [SerializeField] protected float currentVelocity;
+    protected Vector3 movementDirection;
 
     private void Awake()
     {
@@ -18,7 +20,31 @@ public class AgentMovement : MonoBehaviour
 
     public void MoveAgent(Vector2 movementInput)
     {
-        Vector3 movement = new Vector3(movementInput.x, 0f, movementInput.y);
-        rigidBody.velocity = movement.normalized * currentVelocity;
+        if (movementInput.magnitude > 0)
+        {
+            Vector3 movement = new Vector3(movementInput.x, 0f, movementInput.y);
+            //if (Vector2.Dot(movement, movementDirection) < 0) { currentVelocity = 0; }
+            movementDirection = movement.normalized;
+        }
+        currentVelocity = CalculateSpeed(movementInput);
+    }
+
+    private float CalculateSpeed(Vector2 movementInput)
+    {
+        Debug.Log(movementInput.magnitude > 0);
+        if (movementInput.magnitude > 0)
+        {
+            currentVelocity += MovementData.acceleration * Time.deltaTime;
+        }
+        else
+        {
+            currentVelocity -= MovementData.deceleration * Time.deltaTime;
+        }
+        return Mathf.Clamp(currentVelocity, 0f, MovementData.maxSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = currentVelocity * movementDirection;
     }
 }
